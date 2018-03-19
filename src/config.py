320dae02545b51a6a90de477f83bfdc81fe701bd
Surya Prabhakar <suryaprabhakar@Suryas-MacBook-Air.local>
2018-03-18 21:02:14 -0400

    Added config file with email

---
+++ src/config.py
@@ -0,0 +1,8 @@
+# modified by whoever is deploying the application on their own servers AKA us if we are the owner of the website
+
+DEBUG = True
+ADMINS = frozenset([
+    "sprabhakar1820@gmail.com",
+    "sp296@duke.edu"
+]) # a set is a list that has no duplicate values and is unordered
+# frozenset is a Python built in data structure; you cannot add/remove any elements from the set
